6ce02b7dbb785041214d4daabea0ba55b1ffd92e
codebedo <bedirahan13@gmail.com>
2024-12-23 17:12:55 +0300

    This is first example

---
+++ ObjecktoreintedFirst.cpp
@@ -0,0 +1,27 @@
+// object oriented programmiþng
+
+#include <iostream>
+
+using namespace std;
+
+
+class Otomobil {
+	public:
+		string colour;
+};
+
+int main(){
+	
+	
+	Otomobil otomobil1;
+	Otomobil otomobil2, otomobil3;
+	
+	
+	otomobil1.colour = "black";
+	otomobil2.colour = "white";
+	otomobil3.colour = "blue";
+	
+	cout << "oto1 colour " << otomobil1.colour << endl;
+	cout << "oto2 colour " << otomobil2.colour << endl;
+	cout << "oto3 colour " << otomobil3.colour << endl;
+}
