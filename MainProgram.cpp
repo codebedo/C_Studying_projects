31f6e4c427f1ea479f4798c0558ea048344d6ac6
codebedo <bedirahan13@gmail.com>
2024-12-23 18:19:12 +0300

    ı have tı change the IDE

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -3,20 +3,22 @@
 
 using namespace std;
 
-int main(){
-	
-	
-	Otomobil otomobil1, otomobil2 ;
-	
-	otomobil1.colour ="black";
-	otomobil2.colour = "white";
-	
-	
-	cout << otomobil1.colour << endl;
-	cout << otomobil2.colour << endl;
-	
-	
-	cout << endl;
-	return 0;
-	
+int main() {
+    Otomobil otomobil1, otomobil2;
+
+    otomobil1.colour = "black";
+    otomobil1.HorsePower = 90;
+    otomobil1.model = "Renault";
+
+    otomobil2.colour = "dark blue";
+    otomobil2.HorsePower = 140;
+    otomobil2.model = "Passat";
+
+    otomobil1.ShowRegistrationInformation();
+    cout << endl << endl;
+    otomobil2.ShowRegistrationInformation();
+
+    cout << endl;
+    return 0;
 }
+
