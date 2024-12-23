cc7cc6a629e4485ac2edcb13326ca8b8afb97f6b
codebedo <bedirahan13@gmail.com>
2024-12-23 18:20:23 +0300

    fucntions is better

--- Otomobil..cpp
+++ Otomobil..cpp
@@ -1,1 +1,10 @@
-# 
+#include <iostream>
+#include "Otomobil.h"
+
+
+
+void Otomobil::ShowRegistrationInformation() {
+	cout << "Model: " << Otomobil::model << endl;
+	cout << "Renk: " << Otomobil::colour << endl;
+	cout << "Beygir G: " << Otomobil::HorsePower << endl;
+}
