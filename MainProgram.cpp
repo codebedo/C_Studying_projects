c9a24eb0ac331ba68272b2d3c06aad785dc527d5
codebedo <bedirahan13@gmail.com>
2024-12-23 17:50:36 +0300

    include header file

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include "Otomobil.h"
 
 using namespace std;
 
 int main(){
 	
+	
+	Otomobil otomobil1, otomobil2 ;
+	
+	otomobil1.colour ="black";
+	otomobil2.colour = "white";
+	
+	
+	cout << otomobil1.colour << endl;
+	cout << otomobil2.colour << endl;
+	
+	
+	cout << endl;
+	return 0;
 	
 }
