0a32452357ff931c48efc4425e46c4c48c6a9266
codebedo <bedirahan13@gmail.com>
2024-12-24 16:50:47 +0300

    Pointers practacing

--- PontersExamples.cpp
+++ PontersExamples.cpp
@@ -18,13 +18,25 @@ int main(){
 	cout << p <<" " << &x <<" " << *p <<" "<< x << endl;
 	return 0;*/
 	
+	
 	// EXAMPLE 3
+	/*
 	int x = 40; // Declare and initalize an integer variable
 	int *p = &x; // Declare and initalize a pointer to the address of 'x'
 	
 	cout << p << endl; // Print the address stored in the pointer
 	cout << *p << endl; // Print the value stored at the address in the pointer
+	*/
 	
+	int *ptr  = nullptr; // ptr is now a null pointer
+	
+	if (ptr != nullptr){
+		
+		*ptr = 10;
+	}
+	
+	
+	cout << ptr << endl;
 	return 0;
 }
 
