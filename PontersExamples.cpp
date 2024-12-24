867138edc8fbdacbaf7cf213f4f8485f840ccdc5
codebedo <bedirahan13@gmail.com>
2024-12-24 16:41:16 +0300

    Pointers practacing 01

--- PontersExamples.cpp
+++ PontersExamples.cpp
@@ -3,9 +3,17 @@
 using  namespace std;
 
 int main(){
-	
+	/*
 	int x =42; // Declare and initalize an integere variable
 	cout << &x; // Print the address of the variable x
+	return 0;
+	*/
+	
+	int x = 40;
+	int *p;
+	p = &x;
+	
+	cout << p <<" " << &x <<" " << *p <<" "<< x << endl;
 	return 0;
 }
 
