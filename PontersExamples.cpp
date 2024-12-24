a1f0d21abfd544c36860476286e496af7b97eedf
codebedo <bedirahan13@gmail.com>
2024-12-24 16:36:28 +0300

    Pointers practacing

---
+++ PontersExamples.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using  namespace std;
+
+int main(){
+	
+	int x =42; // Declare and initalize an integere variable
+	cout << &x; // Print the address of the variable x
+	return 0;
+}
+
+
