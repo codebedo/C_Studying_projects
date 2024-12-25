4e44f253739517d441a90d352ad40bad138d2a14
codebedo <bedirahan13@gmail.com>
2024-12-25 16:53:36 +0300

    Example 8 with arrays

---
+++ Pointer_examples8.0.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	
+	
+	float arr[5];
+	
+	// Insert data using pointer notaiton
+	
+	cout << "Enter 5 numbers: " ;
+	
+	for(int i = 0; i < 5; ++i) {
+		
+		// store input number in arr[i]
+		cin >> *(arr + i);
+		
+	}
+	
+	
+	// Display data using pointer notation
+	
+	cout << "Displaying data: " << endl;
+	
+	for(int i = 0 ; i < 5; ++i) {
+		
+		// display value of arr[i]
+		
+		cout << *(arr +i) << endl;
+
+	}
+	
+	return 0;
+}
