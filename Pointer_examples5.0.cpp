c07efc46f48f4a25b172f26c5177e86a6d5ac953
codebedo <bedirahan13@gmail.com>
2024-12-25 16:28:21 +0300

    Example 5

---
+++ Pointer_examples5.0.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int var = 5;
+	
+	
+	// store address of var
+	int* point_var = &var;
+	
+	// Print value of var
+	cout << "var = " << var << endl;
+	
+	
+	// print address of var
+	cout << "Address of var (&var)" << &var << endl
+		 <<endl;
+		 
+	// print pointer point_var
+	
+	cout << "Point_var = " << point_var << endl;
+	
+	
+	// print  the content of the address point_var points to
+	
+	cout << "Content of the address pointed to by point_var(*point_vat) = " << *point_var << endl;  
+}
