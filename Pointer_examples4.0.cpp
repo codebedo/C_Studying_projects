1f8343e6fbb026ebd3d4a69c00939eda296b742e
codebedo <bedirahan13@gmail.com>
2024-12-25 16:19:23 +0300

    Example 4

---
+++ Pointer_examples4.0.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+	
+	//declare variables
+	
+	int var1 = 3;
+	int var2 = 24;
+	int var3 = 17;
+	
+	
+	// print address of var 1
+	
+	cout << "Address of var1: " << &var1 << endl;
+	
+	
+	// print address of var 2 
+
+	cout << "Address of var2 : " << &var2 << endl;
+	
+	// Print address of var 3
+	
+	cout << "Address of var3 : " << &var3 << endl;
+	
+	
+}
