800c587078c1ce20384bb8f7e02fe62d29564d13
codebedo <bedirahan13@gmail.com>
2024-12-26 14:39:58 +0300

    Pointers  & struct & functions

---
+++ Pointer_examples15.0.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct Distance {
+	
+	int feet;
+	float inch;
+	
+	void print_distance() {
+		cout << "Displaying Information." << endl;
+		cout << "Distance = " << feet << " feet " << inch << "inches" ;
+		
+	}
+};
+
+int main() {
+	Distance d;
+	Distance* ptr = &d;
+	cout << "Enter feet. " ;
+	cin>> ptr->feet;
+	cout << "Enter inch: ";
+	cin >> ptr->inch;
+	
+	
+	ptr->print_distance();
+	
+	return 0;
+}
