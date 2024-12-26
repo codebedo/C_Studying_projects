4fbe01539be3b86d5e562b2bca8aace35ef30523
codebedo <bedirahan13@gmail.com>
2024-12-26 13:47:49 +0300

    Pointer with stucture

---
+++ Pointer_examples13.0.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+struck Distance {
+	
+	
+	int feet;
+	float inch;
+	
+
+};
+
+
+
+int main() {
+	
+	Distance d;
+	
+	Distance* ptr = &d;
+	
+	
+	cout << "Enter feet: ";
+	cin >> (*ptr).feet;
+	cout << "Enter inch: ";
+	cin >> (*ptr).inch;
+	
+	
+	cout << "Displaying information." << endl;
+	cout << "Distance = " << (*ptr).feet << " feet " << (*ptr).inch << " inches";
+	
+	return 0;
+}
