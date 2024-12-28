863d23705e70b1b6c2fc3aab00ddf7d599f78c75
codebedo <bedirahan13@gmail.com>
2024-12-28 20:11:55 +0300

    Header file for Header tests

---
+++ Ritesh.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+ using namspace std;
+ 
+ namespace Ritesh 
+    {
+        int a;
+        int b;
+        
+        void sum();
+    }
+void Ritesh::sum()
+    {
+        cout <<a+b << endl;
+    }
