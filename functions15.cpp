455751a7158f04d767d8471cce6a1e5af7591a4a
codebedo <bedirahan13@gmail.com>
2025-01-01 20:30:17 +0300

    practacing functions

---
+++ functions15.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+//declating a function
+
+
+void greet(){
+    
+    cout << "HEllo there ";
+    
+}
+
+int main() {
+    
+    // calling the function
+    
+    greet();
+    
+    
+    return 0;
+    
+    
+}
