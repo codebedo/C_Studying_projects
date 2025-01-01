f328d23c3b58abedb958a35a47ed1dfb2f0f0abc
codebedo <bedirahan13@gmail.com>
2025-01-01 20:34:29 +0300

    practacing functions third

--- functions15.cpp
+++ functions15.cpp
@@ -15,11 +15,21 @@ void printNum(int num) {
   cout << num;   
 }
 
+void displayNum(int n1, float n2){
+    cout << "THe int number is " << n1;
+    cout << "THe double number is " << n2;
+    
+}
 int main() {
     
     // calling the function
     
     int n = 7;
+    
+    int num1 = 5;
+    int num2 = 5.5;
+    
+    displayNum(num1,num2);
     
     
     printNum(n);
