de93cdf56556ceca0b83b24623d91583ee870e46
codebedo <bedirahan13@gmail.com>
2025-01-02 15:27:48 +0300

    Question 09

---
+++ Question09.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    const int LENGTH = 21;
+    char message[LENGTH];
+    int i;
+    cout << "Enter a sentence on the line below." << endl;
+    cin >> message[0];
+    
+    i = 0;
+     while (i < LENGTH - 1 && message[i] != '\n')
+     {
+         ++i;
+         cin >> message[i];
+     }
+     message[i] = '\0'; // Replace '\n' if that was read.
+     // Otherwise replace the last character read.
+     cout << message << endl;
+        
+ 
+ 
+ 
+}
