06a4185fdf27f49ebe59dbb040aa6d9d10e97596
codebedo <bedirahan13@gmail.com>
2025-01-18 20:17:44 +0300

    This file missing

--- OtomobilOTO.cpp
+++ OtomobilOTO.cpp
@@ -1,2 +1,18 @@
-#include "OtomobilOTO.h"
+#include <iostream>
+#include "Otomobil.h"
+    // Otomobil constructer
+    Otomobil::Otomobil(string _colour, string _model, int _HorsePower) {
+         colour = _colour;
+         model = _model;
+         HorsePower = _HorsePower;
+         
+    }    
+        
+        
 
+
+void Otomobil::ShowRegistrationInformation() {
+	cout << "Model: " << Otomobil::model << endl;
+	cout << "Renk: " << Otomobil::colour << endl;
+	cout << "Beygir G: " << Otomobil::HorsePower << endl;
+}
