d08719482179e20de4cf2a3e5a3971bd23a4f019
codebedo <bedirahan13@gmail.com>
2025-01-18 20:19:20 +0300

    constructer look it

--- Otomobil..cpp
+++ Otomobil..cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include "Otomobil.h"
-
+    // Otomobil constructer
+    Otomobil::Otomobil(string _colour, string _model, int _HorsePower) {
+         colour = _colour;
+         model = _model;
+         HorsePower = _HorsePower;
+         
+    }    
+        
+        
 
 
 void Otomobil::ShowRegistrationInformation() {
