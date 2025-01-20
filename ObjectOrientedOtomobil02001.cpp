6fab540d41e9c8c7d57112f54af9a5570b3ea5f6
codebedo <bedirahan13@gmail.com>
2025-01-20 20:21:41 +0300

    filling method

--- ObjectOrientedOtomobil02001.cpp
+++ ObjectOrientedOtomobil02001.cpp
@@ -3,6 +3,10 @@
 
 void Otomobil::RegistrationInfo(){
     
+    
+    cout << "Model: "<< Otomobil::model << endl;
+    cout << "Colour: " << Otomobil::colour << endl;
+    cout << "Hp: " << Otomobil::HorsePower << endl;
 
 
 }
