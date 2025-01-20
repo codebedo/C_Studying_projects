815f3bf73209fb5238ed034b15cdc8b633aab332
codebedo <bedirahan13@gmail.com>
2025-01-20 20:02:19 +0300

    Main file

--- ObjectOriented02001.cpp
+++ ObjectOriented02001.cpp
@@ -4,32 +4,13 @@
 using namespace std;
 
 
-class Otomobil{
-    
-    public:
-        
-        string colour;
-        
-        
-};
 
 
 
 int main(){
     
     
-    Otomobil otomobil1;
-    Otomobil otomobil2, otomobil3;
     
-    otomobil1.colour = "black";
-    otomobil2.colour = "white";
-    otomobil3.colour = "blue";
-    
-    
-    
-    cout << "Car colour : " << otomobil1.colour << endl;
-    cout << "Car colour : " << otomobil2.colour << endl;
-    cout << "Car colour : " << otomobil3.colour << endl;
     
     
     cout << endl ;
