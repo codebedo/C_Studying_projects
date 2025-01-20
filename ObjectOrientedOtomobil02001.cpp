cdd2902b48ee1cfb588a53264a31e98d856e9088
codebedo <bedirahan13@gmail.com>
2025-01-20 20:35:01 +0300

    Optimization to code

--- ObjectOrientedOtomobil02001.cpp
+++ ObjectOrientedOtomobil02001.cpp
@@ -3,10 +3,8 @@
 
 void Otomobil::RegistrationInfo(){
     
-    
-    cout << "Model: "<< Otomobil::model << endl;
     cout << "Colour: " << Otomobil::colour << endl;
     cout << "Hp: " << Otomobil::HorsePower << endl;
-
+    cout << "Model: "<< Otomobil::model << endl;
 
 }
