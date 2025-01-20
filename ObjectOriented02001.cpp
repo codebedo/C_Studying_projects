95e740cd2950f6e068d5bc5d8a68444907709386
codebedo <bedirahan13@gmail.com>
2025-01-20 20:13:04 +0300

    Main class  testing 1

--- ObjectOriented02001.cpp
+++ ObjectOriented02001.cpp
@@ -14,6 +14,14 @@ int main(){
     
     
     
+    Otomobil  otomobil1, otomobil2;
+    
+    otomobil1.colour = "black";
+    otomobil2.colour = "white";
+    
+    cout << otomobil1.colour << endl ;
+    cout << otomobil2.colour << endl;
+    
     
     cout << endl ;
     
