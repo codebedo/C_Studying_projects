8358ad5247b5664b69d2ebce171e7ddf8bcbab1e
codebedo <bedirahan13@gmail.com>
2025-01-21 15:59:26 +0300

    oto cons

--- ObjectOrientedOtomobil02001.cpp
+++ ObjectOrientedOtomobil02001.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "ObjectOrientedHeader02001.h"
 
-
-Otomobil::Otomobil(String _colour, int _HorsePower, string _model)
+//otomobil const
+Otomobil::Otomobil(String _colour, int _HorsePower, string _model) {
+    colour = _colour;
+    HorsePower = _HorsePower;
+    model = _model;
+}
 
 
 void Otomobil::RegistrationInfo() {
