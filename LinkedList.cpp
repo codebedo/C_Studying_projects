26d344aa6af5921de390ac51c570753ecc440dd6
Phoneger101 <bedirahan13@gmail.com>
2025-06-05 20:03:32 +0300

    ı am working on it

---
+++ LinkedList.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+using namespace std;
+
+    sturct Node {
+        int data;
+        Node* next;
+        
+        Node(int val) : data(val), next(nullptr) {}
+};
+
+
+class LinkedList {
+    privates:
+        Node* head;
+        
+    public:
+        LinkedList() : head(nullptr) {}
+        
+        
+        // Yeni düðüm ekle (listenin sonuna)
+        
+        void append(int value) {
+            Node* newNode = new Node(value);
+            if (head)  {
+                head = newNode;
+                return;
+                
+        }
+        
+        Node* temp = head;
+        while (temp->next)
+            temp = temp -> next;
+        temp->next = newNode;
+    
