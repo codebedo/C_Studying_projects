087cc37ec9d7cede20f47d605e506bc3120d32f3
codebedo <bedirahan13@gmail.com>
2025-06-26 18:37:50 +0300

    Matrix example

---
+++ MatrixArray.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    
+       int arr[2][3] = {{8, 17, 25}, {21, 23,42,32}};
+       
+       
+       
+       
+       cout << arr[2][4]<< endl;
+       
+       cout << endlİ
+       return 0;
+}
