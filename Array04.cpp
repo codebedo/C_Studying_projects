e8b90eaea48bb40b68b7f613be5e6584c3e961c2
codebedo <bedirahan13@gmail.com>
2025-06-28 20:12:01 +0300

    array reads

---
+++ Array04.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    
+    int arr[] = {1,2,3,5,6};
+    
+    
+    cout << arr[0] << endl;
+    int i;
+    for (i = 0; i <5; i++){
+        cout <<" Arrays element"<< endl;
+        cout << arr[i]<<endl;
+    }
+    
+    return 0;   
+    
+    
+}
