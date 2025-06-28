60456aaa9b6fff05b1a7177e23287e611103cee2
codebedo <bedirahan13@gmail.com>
2025-06-28 14:30:51 +0300

    Array example

---
+++ Array02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    
+    int arr[6] = {1,2,5,6,73,8};
+    for(i = 0: i < arr[] ; i++){
+        
+        cout << arr[i] << endl;
+        
+    }
+    
+    
+}
