6e34cddf539bf40b7efc3fda956aa305d3faf308
codebedo <bedirahan13@gmail.com>
2025-07-01 18:33:18 +0300

    learn better

--- Binary_Search_algorithm.cpp
+++ Binary_Search_algorithm.cpp
@@ -48,6 +48,8 @@ int main(void)
     int arr[] = {2,3,4,5,6,10,40};
     int x = 3;
     int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "sizeof arr" << sizeof(arr) << endl;
+    cout << "siof arr[0] " <<sizeof(arr[3]) << endl; 
     int result = binarySearch(arr, 0, n -1, x);
     if (result == -1) cout << "Element is not present in array";
     else cout << "Element is present at index" << result ;
