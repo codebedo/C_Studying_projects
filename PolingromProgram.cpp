3872fa73337b52f141e2ddace5dfcbceb0087bb8
codebedo <bedirahan13@gmail.com>
2025-07-02 15:46:30 +0300

    this is he worst code ever

--- PolingromProgram.cpp
+++ PolingromProgram.cpp
@@ -2,56 +2,23 @@
 
 using namespace std;
 
-
-
-int oneDigit(int num)
-
-{
-
-	return (num >= 0 && num < 10);
-
-}
-
-bool isPalUtil(int num, int* dupNum)
-{
-	if(oneDigit(num))	
-	   return (num == (*dupNum) % 10);
-
-	
-	if(!isPalUtil(num / 10, dupNum))
-	   return false;
-	
-	*dupNum /= 10;
-
-
-	return (num % 10 == (*dupNum) % 10);
-
-}
-
-int isPal(int num)
-{
-	if (num > 0)
-	    num = -num;
-
-	
-	int* dupNum = new int(num);
-
-	return isPalUtil(num, dupNum);
-}
-
-int main(){
-
-	int n = 12321;
-	isPal(n) ? cout << "Yes":
-		   cout << "No" << endl;
-
-	
-
-	n = 8999;
-	isPal(n) ? cout << "Yes":
-		   cout << "No";
-	
-	
-	return 0;
-
+int main(int argc, const char* argv[]){
+    
+    printf("bir sayi giriniz: ");   
+    
+    
+    int sayi;
+        std:: cin >> sayi;
+            
+            while(sayi > 0) {
+                std::cout << sayi%10 <<std::endl;
+                
+                
+                
+                sayi = sayi/10;
+        
+        }
+    
+    return 0;
+    
 }
