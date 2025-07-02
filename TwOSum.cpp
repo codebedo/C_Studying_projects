de19d2b89524146469b79625a6764498dedf69e4
codebedo <bedirahan13@gmail.com>
2025-07-02 14:38:14 +0300

    This is one-pass hash map solution

--- TwOSum.cpp
+++ TwOSum.cpp
@@ -63,6 +63,21 @@ class Solution {
         
     };
     
+class OneMapSolution {
+    public:
+        
+        vector<int> twoSum(vector<int>&  nums, int target){
+            unordered_map<int, int> numMap;
+            for(int i = 0; i < nums.size(); i++){
+                int complement = target - nums[i];
+                if (numMap.count(complement)) {
+                    return {numMap[complement], i};
+                }
+                numMap[nums[i]] = i;
+            }
+            return {};
+        }
+    };
     
     int main(){
         
@@ -71,10 +86,12 @@ class Solution {
         
         
         
-        Solution solution;
+        //Solution solution;
+        OneMapSolution fastsolution;
         
         
-        vector<int> result = solution.twoSum(nums, target);
+        //vector<int> result = solution.twoSum(nums, target);
+        vector<int> result = fastsolution.twoSum(nums, target);
         
         
         if(!result.empty()){
