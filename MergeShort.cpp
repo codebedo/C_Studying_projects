810a3f9e9a4ee16e14648e7a722152b3be834d1b
codebedo <bedirahan13@gmail.com>
2025-07-05 16:20:30 +0300

    Learning algorithm

---
+++ MergeShort.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Merges two subarrays of arr[]
+// first subarray is arr[left.mid]
+// second subarray is arr[mid..right]
+
+
+void merge(vector<int>& arr, int left, int mid, int right)
+{
+    
+    int n1 = mid - left + 1;
+    int n2 = right - mid ;
+    
+    // create temp vectors
+    vector<int> L(n1), R(n2);
+    
+    
+    // copy data to temp vectors L[] and R[]
+    
+    for(int i = 0; i <n1 ; i++)
+        L[i] = arr[left + i];
+        
+    for(int j = 0; j< n2; j++)
+        R[j] = arr[mid+ 1 + j];
+        
+    int i = 0, j = 0;
+    int k = left;
+    
+    // Merge the temp vectors back
+    // into arr[left..right]
+    while(i< n1 && j < n2){
+        if(L[i] <= R[j]){
+            arr[k] = L[i];
+            i++;
+        }    
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    // copy tye reamining elements of L[]
+    // if theere are any
+    
+    while(i < n1) {
+            
+        arr[k] = L[i];
+        i++;
+        k++;    
+        
+        
+    }    
+}
+
+// begin is for left index and end is right index
+// of the subarray of arr to be sorted
+
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left >= right)
+        return;
+        
+    int mid = left + (right -left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left , mid, right);
+    
+
+
+
+}
+
+
+
+// Function print vector
+
+void printVector(vector<int>& arr)
+{
+    for(int i = 0; i<arr.size(); i++)
+        cout << arr[i] << " ";
+    cout << endl;
+        
+}
+
+// Driver Code
+
+int main()
+{
+    vector<int> arr = {12, 11,13,5, 6,7};
+    int n = arr.size();
+    
+    cout << "Given vector is \n ";
+    printVector(arr);
+    
+    mergeSort(arr, 0, n - 1);
+    
+    cout << "\n Sorted vector is \n";
+    printVector(arr);
+    
+    return 0;   
+}
