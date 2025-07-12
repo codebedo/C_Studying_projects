aca1f9a7a0892ba1c5aea41589db5a4fcd1027fb
codebedo <bedirahan13@gmail.com>
2025-07-12 18:20:14 +0300

    C++ learning

--- Udemy_C++_lesson/FunctionParapemeter.cpp
+++ Udemy_C++_lesson/FunctionParapemeter.cpp
@@ -10,11 +10,22 @@ void changeToArray(int arr[], int size){
     }
     
 }
+void ChanteTonode(int arr[], int size){
+    int  i = size;
+    while( i >= size) {
+        i -= 1;
+        cout<< arr[i] << " ";
+        
+    }   
+}
+
+
 int main(){
     
     int arrr[] = {1,2,3,4,5,6,7};
     
     changeToArray(arrr, 8);
+    ChanteTonode(arrr, 8);
     
     for(int i = 0; i < 7; i++){
         cout << arrr[i] << " , " ;
