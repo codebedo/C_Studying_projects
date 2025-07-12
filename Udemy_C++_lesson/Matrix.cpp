3df157375e788d7e6f422b1bf46c0e55f344ba21
codebedo <bedirahan13@gmail.com>
2025-07-12 16:22:34 +0300

    this is for Udemy lesson

---
+++ Udemy_C++_lesson/Matrix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    
+    
+    int arr[2][3] = {{2,25, 25},{12,15,16} };
+    
+    
+    
+    cout << arr[0][1];
+    cout << endl;
+    return 0;
+    
+    }
