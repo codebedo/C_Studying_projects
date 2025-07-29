d44dcf6d0bfa500e19895ce807a485ce310a7f1d
codebedo <bedirahan13@gmail.com>
2025-07-29 20:09:53 +0300

    this is for vector

---
+++ LearnVector.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#inclue <vector>
+
+using namespace std;
+
+
+int main(){
+    
+    /*creating an empty vector
+    
+    
+    vector<int> v1;
+    
+    return 0;    
+    */
+    
+    
+}
