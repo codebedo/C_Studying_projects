20f8093833d204d846c5cd845dfba4c824d24e3f
codebedo <bedirahan13@gmail.com>
2025-07-29 19:42:09 +0300

    LeetCode Problems

---
+++ LeetCode/LongestComman.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main(){
+        
+        
+      string arr[3] = {"flower", "flow", "fliht"};
+      
+      
+      cout << arr[1] ;    
+    
+}
+
+
