315e442ee874702faeb6bf5a86c25c4a673ddca8
codebedo <bedirahan13@gmail.com>
2025-07-29 18:59:10 +0300

    triying better

--- SearchAlgorithm/QuickSort.cpp
+++ SearchAlgorithm/QuickSort.cpp
@@ -1,53 +1,80 @@
-#include <bits/stdc++.h>
+// Quick sort in C++
+
+#include <iostream>
 using namespace std;
 
-int partition(vector<int>& arr, int low, int high) {
-  
-    // Choose the pivot
-    int pivot = arr[high];
-  
-    // Index of smaller element and indicates 
-    // the right position of pivot found so far
-    int i = low - 1;
+// function to swap elements
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
 
-    // Traverse arr[low..high] and move all smaller
-    // elements on left side. Elements from low to 
-    // i are smaller after every iteration
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
-        }
-    }
+// function to print the array
+void printArray(int array[], int size) {
+  int i;
+  for (i = 0; i < size; i++)
+    cout << array[i] << " ";
+  cout << endl;
+}
+
+// function to rearrange array (find the partition point)
+int partition(int array[], int low, int high) {
     
-    // Move pivot after smaller elements and
-    // return its position
-    swap(arr[i + 1], arr[high]);  
-    return i + 1;
-}
-
-// The QuickSort function implementation
-void quickSort(vector<int>& arr, int low, int high) {
+  // select the rightmost element as pivot
+  int pivot = array[high];
   
-    if (low < high) {
+  // pointer for greater element
+  int i = (low - 1);
+
+  // traverse each element of the array
+  // compare them with the pivot
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+        
+      // if element smaller than pivot is found
+      // swap it with the greater element pointed by i
+      i++;
       
-        // pi is the partition return index of pivot
-        int pi = partition(arr, low, high);
-
-        // Recursion calls for smaller elements
-        // and greater or equals elements
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+      // swap element at i with element at j
+      swap(&array[i], &array[j]);
     }
+  }
+  
+  // swap pivot with the greater element at i
+  swap(&array[i + 1], &array[high]);
+  
+  // return the partition point
+  return (i + 1);
 }
 
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+      
+    // find the pivot element such that
+    // elements smaller than pivot are on left of pivot
+    // elements greater than pivot are on righ of pivot
+    int pi = partition(array, low, high);
+
+    // recursive call on the left of pivot
+    quickSort(array, low, pi - 1);
+
+    // recursive call on the right of pivot
+    quickSort(array, pi + 1, high);
+  }
+}
+
+// Driver code
 int main() {
-    vector<int> arr = {10, 7, 8, 9, 1, 5};
-    int n = arr.size();
-    quickSort(arr, 0, n - 1);
+  int data[] = {8, 7, 6, 1, 0, 9, 2};
+  int n = sizeof(data) / sizeof(data[0]);
   
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    return 0;
+  cout << "Unsorted Array: \n";
+  printArray(data, n);
+  
+  // perform quicksort on data
+  quickSort(data, 0, n - 1);
+  
+  cout << "Sorted array in ascending order: \n";
+  printArray(data, n);
 }
