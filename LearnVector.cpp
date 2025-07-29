3ae7c838653714e2e6a973f31ba0d3a498c8daa9
codebedo <bedirahan13@gmail.com>
2025-07-29 20:16:12 +0300

    trying examples

--- LearnVector.cpp
+++ LearnVector.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
-
-#inclue <vector>
-
+#include <vector>
 using namespace std;
+void printVector(vector<int>& v){
+        for(auto x: v){
+            cout << x << " ";
+        }    
+        
+        
+        cout << endl;
+}
 
 
 int main(){
     
-    /*creating an empty vector
+    // Creating a vector of 5 elements from
+    // initializer list
+    vector<int> v1 = {1, 4, 2, 3, 5};
+    
+    //Creating a vector of 5 element with
+    // default value
+    vector<int> v2 (5,9);
+    
+    printVector(v1);
+    printVector(v2);
+    
+    return 0;
     
     
-    vector<int> v1;
     
-    return 0;    
-    */
     
     
 }
+
