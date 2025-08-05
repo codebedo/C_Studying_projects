df79b2464094fbb17a060ae851b0ee77edd1fa18
codebedo <bedirahan13@gmail.com>
2025-08-05 19:16:57 +0300

    pointer stack & heap

--- Udemy_C++_lesson/StackandHeap.cpp
+++ Udemy_C++_lesson/StackandHeap.cpp
@@ -14,14 +14,26 @@ void Food(){
 
 void bar() {
         int* p = new int(20);
-        cout<< "p memory reference value: " << *p << endl;
+        cout<< "p memory reference value: " << p << *p << endl;
+        delete p ;
+        
+        
+        cout << *p << endl;
         
     }
 int main(){
     
     
-    Food();
-    bar();
+    int x, y;
+    int z = 10;
+    int* p = new int(5);
+    delete p;
+    
+    int w = 20;
+    p = &w;
+    cout << *p << endl;
+    
+    
     
     cout << endl;
     return 0;    
