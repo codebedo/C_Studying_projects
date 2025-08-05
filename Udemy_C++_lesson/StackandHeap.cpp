810e2b747490f51299178e49bfd15fad821cafdf
codebedo <bedirahan13@gmail.com>
2025-08-05 19:09:28 +0300

    this for stack optimization

--- Udemy_C++_lesson/StackandHeap.cpp
+++ Udemy_C++_lesson/StackandHeap.cpp
@@ -10,11 +10,18 @@ void Food(){
     cout << "x : " << x << endl;
     }
 
+
+
+void bar() {
+        int* p = new int(20);
+        cout<< "p memory reference value: " << *p << endl;
+        
+    }
 int main(){
     
     
     Food();
-    
+    bar();
     
     cout << endl;
     return 0;    
