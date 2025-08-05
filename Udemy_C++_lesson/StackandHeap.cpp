5aa14efbdccea48af00769ffd36a285cdd8a1d18
codebedo <bedirahan13@gmail.com>
2025-08-05 15:46:53 +0300

    this is stack and heap example

---
+++ Udemy_C++_lesson/StackandHeap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+void Food(){
+    
+    int x = 10;
+    cout << "x : " << x << endl;
+    }
+
+int main(){
+    
+    
+    Food();
+    
+    
+    cout << endl;
+    return 0;    
+}
