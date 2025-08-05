e4e587f7691903a671cd49adfa67624185b448b0
codebedo <bedirahan13@gmail.com>
2025-08-05 13:56:47 +0300

    we have to learn

---
+++ Udemy_C++_lesson/TryCatch.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    
+    
+    int bolen , bolunen;
+    
+    
+    cout << "please enter the diveding number" << endl;
+    cin >> bolen ;
+    
+    
+    cout << "please enter the dived number " << endl;
+    cin >> bolunen;
+    
+    
+    int sonuc = bolunen / bolen ;
+    
+    cout << "sonuc : " << sonuc  << endl ;
+        
+    
+}
