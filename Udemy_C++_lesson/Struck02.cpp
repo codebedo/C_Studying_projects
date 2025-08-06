a8e36b9a0fdc0d9d6727ef5d979b323f05edd028
codebedo <bedirahan13@gmail.com>
2025-08-06 20:25:09 +0300

    new day new code yeahhhhhhhhhhh

--- Udemy_C++_lesson/Struck02.cpp
+++ Udemy_C++_lesson/Struck02.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iostream>
 
 using namespace std;
 
@@ -13,8 +14,32 @@ struct Student{
 
 int main(){
     
-    Student student;
+    Student students[3];
     
+    
+    for(int i = 0; i < 3; i++){
+        
+            cout<< i + 1 << ".student's no : ";
+            cin >> students[i].student_no;
+            cin.ignore(); // for the clean up the new line 
+            cout << i + 1 << ".students name: ";
+            getline(cin, students[i].name);
+            cout << i + 1 << ".students score: ";
+            cin >> students[i].PointScore;
+            
+            
+        }
+    
+    
+    
+    for(int i = 0; i < 3; i++){
+            cout << students[i].student_no << " " << students[i].name << " " << 
+            students[i].PointScore << endl;
+        }
+    
+    
+    
+    /*
     student.student_no = 303;
     student.name = "Bedirhan Aydin";
     student.PointScore = 3.00;
@@ -24,6 +49,6 @@ int main(){
     
     Student student1 = {302, "Onur Tanis", 3.50};
     
-    cout << student1.student_no << " " << student1.name << " " << student.PointScore << endl;
+    cout << student1.student_no << " " << student1.name << " " << student.PointScore << endl;*/
     
 }
