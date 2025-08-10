0547f317afb5c0dabb690409e4ee2de1199e2aec
codebedo <bedirahan13@gmail.com>
2025-08-10 11:55:35 +0300

    this is for simple example

---
+++ Udemy_C++_lesson/repetaing.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+        
+        
+        int arr[3] = {1, 2,3};
+        
+        
+        for(int i = 0; i < 3; i++)
+        {
+            cout << arr[i] + 1 << endl;
+            
+                
+        }
+        
+        
+        
+        cout << endl;
+        return 0;
+        
+        
+    
+    
+    }
