5a5061cd03313e51749c8a71ce5a7ea567da3563
codebedo <bedirahan13@gmail.com>
2025-08-15 20:36:43 +0300

    this is kodlab book example

---
+++ KBExamples.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main(){
+    
+    char ad[10], soyad[20];
+    
+    
+    cout << "Please enter your name" << endl;
+    gets(ad);
+    
+    cout << "Please enter your surname" << endl;
+    gets(soyad);
+    
+    
+    cout << ad  <<" "<<  soyad << endl;
+    
+    
+    cout << endl;
+    return 0;    
+    
+    
+}
