706915aedf0f6d492a052d730c0ceb95c4d802f3
codebedo <bedirahan13@gmail.com>
2025-08-21 22:17:00 +0300

    oop examples private & public

--- Udemy_C++_lesson/OOP_Eample/Otomobil.h
+++ Udemy_C++_lesson/OOP_Eample/Otomobil.h
@@ -6,12 +6,23 @@ using namespace std;
 
 
 class Otomobil {
-public:
+public:/*
     string renk;
     string model;
-    int hp;
+    int hp;*/
     
     
     void PapersInfo();
+    // Constructer functions
+    Otomobil(string _renk,string _model , int _hp);
+    void setModelYear(int _my);
+    int getModelYaar();
+    void setOtomobilRenk(string _renk);
+    void getOtomobilRenk();
+// Private Tests
 
+private:
+     string renk;
+     string model;
+     int hp;
 };
