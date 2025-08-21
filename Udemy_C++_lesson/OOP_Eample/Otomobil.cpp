62ae012fcb20fac1db459cab3da7ecc0f9a7adcf
codebedo <bedirahan13@gmail.com>
2025-08-21 22:16:35 +0300

    oop examples private & public

--- Udemy_C++_lesson/OOP_Eample/Otomobil.cpp
+++ Udemy_C++_lesson/OOP_Eample/Otomobil.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include "Otomobil.h"
+// Otomobil Consturacter
+Otomobil::Otomobil(string _renk,string _model , int _hp){
+    renk = _renk;
+    model = _model;
+    hp = _hp;
+
+}
 
 void Otomobil::PapersInfo(){
     cout <<" Renk:  " << Otomobil::renk << endl;
     cout <<" Model:  "<< Otomobil::model << endl;
     cout <<"Beygir G : " << Otomobil::hp << endl;    
+    
+}
+
+void Otomobil::setOtomobilRenk(string _renk){
+    renk = _renk;    
+}
+
+string Otomobil::getOtomobilRenk(){
+    return renk;    
     
 }
