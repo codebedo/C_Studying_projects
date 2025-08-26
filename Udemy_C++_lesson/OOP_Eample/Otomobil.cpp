cef1c394469fc293a7c03a43d1eafc990618f67e
codebedo <bedirahan13@gmail.com>
2025-08-26 19:03:25 +0300

    pointers object

--- Udemy_C++_lesson/OOP_Eample/Otomobil.cpp
+++ Udemy_C++_lesson/OOP_Eample/Otomobil.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include "Otomobil.h"
-// Otomobil Consturacter
-Otomobil::Otomobil(string _renk,string _model , int _hp){
+// Otomobil Consturactor
+Otomobil::Otomobil(string _renk,string _model , int _hp, int dc){
+    cout << "constructor has been called" << endl;
     renk = _renk;
     model = _model;
     hp = _hp;
+    pDoorCount = new int(dc);  // *pDoorCount = dc; same thing...
 
 }
-
+// """DESTRUCTORS (C++ only)"""
+    // destructor are usually used to
+    // deallocate memory and do other cleanup for a classes object and its class members when the object is destroyed.
+    // A destuctor is called for a class object when that object passes out of scope or is explicitly deleted.
+// Otomobil Destructor
+Otomobil::~Otomobil(){
+    // if we want to which one is called just typing model...
+    cout << Otomobil::model<< "  " << "Destructor has been called" << endl;
+    
+}
 void Otomobil::PapersInfo(){
     cout <<" Renk:  " << Otomobil::renk << endl;
     cout <<" Model:  "<< Otomobil::model << endl;
     cout <<"Beygir G : " << Otomobil::hp << endl;    
+    cout <<"Door count : " << *(Otomobil::pDoorCount) << endl;
     
 }
 
