3ed7283835cdbf7a8e6d2122e7392c1d9e3fcd41
codebedo <bedirahan13@gmail.com>
2025-10-09 18:51:25 +0300

    This is leetcode solution waiting for updates

---
+++ StringCompare.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+class Solution{
+
+
+public: 
+    string longestCommonPrefix(vector<string>& strs){
+        
+    }
+
+};
+int main(){
+    
+ strs
+ #string str1("fly");
+ #string str2("Geeks");
+ 
+ // Compares 5 characters starting from index
+ 
+ // 3 of str1 with str2
+     
+     if((str1.compare(3, 5, str2)) == 0)
+        cout << "Substring Matched" ;
+     
+     else
+        cout << "Strings Not Matched";   
+    
+    return 0;
+        
+}
