6d9b582da304457123b6bb7b03ca405bfad60586
codebedo <bedirahan13@gmail.com>
2025-10-10 19:01:32 +0300

    vector checking

--- VectorAlg.cpp
+++ VectorAlg.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 
 int main(){
-    
+    /*
     vector<char> v = {'a', 'c', 'f', 'd', 'z'};
     
     // Deleting last element 'z'
@@ -21,6 +21,22 @@ int main(){
         cout<< v[i] << " " ;
             
     }
+*/
 
+    vector<int>v;
+    
+    // Check if the vector is empty
+    
+    if(v.empty()){
+        cout << "Vector is empty" <<endl;    
+    }
+    
+    
+    //add an element
+    v.push_back(100);
+    if(!v.empty()){
+        
+        cout << "vector is not empty. first element " << v[0] << endl;    
+    }
     return 0;
 }
