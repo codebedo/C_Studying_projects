973197d8ab17f4f068c87beca90c72f3b86caf33
codebedo <bedirahan13@gmail.com>
2025-10-11 19:23:47 +0300

    updated for example 6

--- VectorExmp.cpp
+++ VectorExmp.cpp
@@ -126,7 +126,7 @@ int main(){
         cout << i << " " ;    
     }
     /*******Example 3 Ending ***********/
-    
+    /*******Example 4 starting**********
     
     vector<int> num{1,2,3,4,5};
     
@@ -135,10 +135,84 @@ int main(){
     cout << "Element index 2 : " << num.at(2) << endl;
     cout << "Element index 3 : " << num.at(3) << endl;
     cout << "Element index 4 : " << num.at(4) << endl;
+    /*******Example 4 ending**********/
+    
+    /******Example 5 starting*********
+    
+    vector<int> num {1,2,3,4,5,8,9};
+    
+    cout << "\nInýtial vector";
+    
+    for(const int& i : num ){
+        
+        cout << i << " " ;    
+    }
+    
+    
+    // change elements to 20 and 25
+    
+    num.at(5) = 20;
+    num.at(6) = 25;
+    
+    cout << "\nUpdated vector";
+    
+    for(const int& i : num){
+        
+        cout << i << " " ;    
+    }
+    
+
+    vector<int> num1{1,2,3,4,5,78,90};
+    
+    cout << "\nInitial vector " ;
+    
+    for(const int& i : num1){
+        
+        cout << i << " " ;
+            
+    }
+    
+    
+    // adding and changed elements 
+    
+    num1.at(2) = 45;
+    num1.push_back(54);
+    
+    
+    cout << "\nUpdated vector" ;
+    
+    for(const int& i : num1){
+        
+        cout << i << " " ;    
+    }    
+    
+    
+    /******Example 5 Ending*********/
     
     
     
+    /******Example 6 starting********
     
+    vector<int> myvector;
+    
+    myvector.push_back(10);
+    
+    cout << "\nInitial vector " ;
+    for(const int& i : myvector){
+            cout << i << "  " ;
+        }
+    while(myvector.back() != 0)
+    {
+        myvector.push_back(myvector.back() -2);
+    }
+    
+    
+    cout << "\nmyvector contains: ";
+    for(const int& i : myvector){
+        cout << i << " " ;
+    }
+    cout << "\n " ;
+    /******Example 6 Ending*********/
 
     
     return 0;
