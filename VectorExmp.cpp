b86e5f301a9f16dca0fee6d95293cfa4d8d1758a
codebedo <bedirahan13@gmail.com>
2025-10-11 18:09:46 +0300

    this is for vector examples 3

--- VectorExmp.cpp
+++ VectorExmp.cpp
@@ -60,6 +60,32 @@ int main(){
     ********* example 2 ending *********/
     
     
+    /*******Example 3 Starting ***********/
+    
+    
+    vector<int> num{1,2,3,4,5};
+    
+    cout << "Initial Vector: ";
+    
+    for(const int& i : num) {
+        cout << i << " ";    
+    }
+    
+    // add the integers 6 and 7 to the vector
+    num.push_back(6);
+    num.push_back(7);
+    
+    
+    cout << "\n updated Vector: ";
+    
+    
+    for(const int& i : num) {
+        
+        cout << i << " ";    
+    }    
+    
+    
+    return 0;
     
     return 0;
     
