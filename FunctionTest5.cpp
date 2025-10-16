3decaca0fdcf5cbbae83029034561a9bd3e23ad2
codebedo <bedirahan13@gmail.com>
2025-10-16 10:57:11 +0300

    functions 07

--- FunctionTest5.cpp
+++ FunctionTest5.cpp
@@ -19,6 +19,14 @@ int Cancel(int total, int canceled){
     return t;
 }
 
+void dublicate (int& a, int& b, int& c){
+    
+    a *= 2;
+    b *= 2;
+    c *= 2;    
+}
+
+
 int main(){
     
     int z;
@@ -32,4 +40,13 @@ int main(){
     subtotal = Cancel(addition(5,3), 3);
     
     cout << "The subtotal is " << subtotal;
+    
+    int x = 1, y =3 , j = 7;
+    
+    dublicate(x,y,j);
+    cout<< "x = " << x << "y = " << y << ", j = " << j;
+    return 0;
+    
 }
+
+
