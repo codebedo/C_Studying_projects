97746cc85fbd901c3a79cd3b0c484d5d231d7ec4
codebedo <bedirahan13@gmail.com>
2025-10-17 18:50:45 +0300

    template function test

--- VectorTrain01.cpp
+++ VectorTrain01.cpp
@@ -22,21 +22,42 @@ int callByVector(int a, int b) {
     }
     
     
+    
+    
 }
 
+// function Templates
+
+template <class T, class U>
+bool are_equal(T a, U b)
+{
+    return (a==b);    
+}
+    
+        
+    
 int main() {
     
+    
+    if (are_equal(10, 10.0))
+      cout << "x and y are equal \n" ;
+    else
+      cout << "x and y are not equal \n";
+        
+    return 0;
+    
+    /*
         
         int Value ;
         int Steep;
         
-        
+     
         cout << "What is the vector step ";
         cin >> Steep ;
         cout<< "What is the value of vector " ;
         cin>> Value;
         
         callByVector(Steep, Value); 
-        
+        */
         
 }
