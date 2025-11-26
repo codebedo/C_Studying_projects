a5f5b67bba251cf7994809424a616d2bc1f76af4
codebedo <bedirahan13@gmail.com>
2025-11-26 20:05:49 +0300

    this is exp assing vecotr

---
+++ vectrAsgn.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+
+    vector<int> first;
+    vector<int> second;
+    vector<int> third;
+    
+    
+    first.assign (7, 100);
+    
+    vector<int>::iterator it;
+    it=first.begin()+1;
+    
+    second.assign(it,first.end() -1);
+    
+    int myints[] = {1776, 7,4};
+    
+    third.assign(myints,myints+3);
+    
+    cout << "size of first : " << int (first.size()) << " \n " ;
+    cout << "size of second : " << int (second.size()) << " \n " ;
+    cout << "size of third : " << int (third.size()) << " \n " ;
+    
+    second.assign(it, first.end() -1);
+    
+    cout << "size of second : " << int (second.size()) << " \n " ;
+    
+    
+    return 0;   
+    
+    
+}
